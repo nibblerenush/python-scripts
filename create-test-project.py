081f867af51cc1336819e7d053ea0265f0f4a878
kshakhayev <kaymran.shakhayev@stoloto.ru>
2019-09-20 17:53:54 +0300

    Added: create-test-project.py

---
+++ create-test-project.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import os
+import sys
+
+def main():
+  dirTarget = sys.argv[1]
+  projectName = sys.argv[2]
+  projectDir = dirTarget + "/" + projectName
+
+  try:
+    os.mkdir(projectDir)
+    os.mkdir(projectDir + "/build")
+  except OSError:
+    print("Creation of the directory %s failed" % projectDir)
+
+  mainCppContent = \
+    "#include <iostream>\n" \
+    "\n" \
+    "int main()\n" \
+    "{\n" \
+    "  return 0;\n" \
+    "}\n" \
+  
+  with open(projectDir + "/main.cpp", 'w') as mainCppFile:
+    mainCppFile.write(mainCppContent)
+  
+  cmakeListsContent = \
+    "cmake_minimum_required(VERSION 3.0.0)\n" \
+    "project(" + projectName + ")\n" \
+    "add_executable(${PROJECT_NAME} main.cpp)\n"
+  
+  with open(projectDir + "/CMakeLists.txt", "w") as cmakeListsFile:
+    cmakeListsFile.write(cmakeListsContent)
+  
+if __name__ == "__main__":
+  main()
--- dircompare.py
+++ dircompare.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import difflib
 import re
 import sys
